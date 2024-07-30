1a23dc14007127d4085758b338c70de9cdb3b20a
Shawn Thebeau <50107354+thebeau1992@users.noreply.github.com>
2024-07-30 19:00:36 -0400

    Update paypal_config.php

--- paypal_config.php
+++ paypal_config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-    'client_id' => 'AfaVRsvvAL_7O-fN9Fwz4rRYEM2uLNEmwnnJVJgBCjG6Kbr4x-dC5hmFg0aIzmYdF5LBIfw4Z0QRnMWJ',
-    'secret' => 'EB5t2bVtg4xAZlxkSDh7fmBpi2PRXVAidRZVpws6z-SB2o5D3V5OFYHR-gZbiRbuSpydIKbogSEwIIs5',
+    'client_id' => '',
+    'secret' => '',
     'settings' => array(
         'mode' => 'live', // 'sandbox' for practice
         'http.ConnectionTimeOut' => 30,
